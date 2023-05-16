c40201dd3b42ae1b59323bc3c4a2b30f9c41daee
André Sampaio <andresampaio@id.uff.br>
2023-05-16 18:01:01 -0300

    PUT and "GET by Id" creation

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,6 +8,19 @@ class LivroController{
           })
     }
 
+    static listarLivroPorId = (req, res)=>{
+        const id = req.params.id;
+        livros.findById(id, (err,livros)=>{
+            if(err){
+                res.status(400).send({message: `${err.message} -  book Id not found.`})
+            }
+            else{
+                res.status(200).send(livros)
+            }
+        })
+          
+    }
+
     static cadastrarLivro = (req, res)=>{
         let livro = new livros(req.body);
         livro.save((err)=>{
@@ -16,6 +29,18 @@ class LivroController{
             }
             else{
                 res.status(201).send(livro.toJSON())
+            }
+        })
+    }
+
+    static atualizarLivro = (req, res)=>{
+        const id = req.params.id;
+        livros.findByIdAndUpdate(id,{$set: req.body}, (err)=>{
+            if(!err){
+                res.status(200).send({message: 'Book successfully updated!'})
+            }
+            else{
+                res.status(500).send({message: `${err.message} -  failed to uptade book.`})
             }
         })
     }
