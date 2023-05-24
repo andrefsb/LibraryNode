36db7b4be7b4a86e3fd6575195806d1e233bc70d
André Sampaio <andresampaio@id.uff.br>
2023-05-24 17:30:45 -0300

    Ordering Get livros result

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -6,13 +6,16 @@ class LivroController{
 
   static listarLivros = async (req, res, next)=>{
     try{
-      let{ limite=5, pagina=1 } = req.query;
+      let{ limite=5, pagina=1 , ordenacao = "_id=-1"} = req.query;
 
+      let [campoOrdenacao, ordem]= ordenacao.split(":");
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if(limite >0 && pagina >0){
         const livrosResultado =  await livros.find()
+          .sort({[campoOrdenacao]: ordem})
           .skip(limite*(pagina-1))
           .limit(limite)
           .populate("autor")
