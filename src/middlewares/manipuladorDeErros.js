b9cee1c22fb542d77661686e2f59ae5c07af0ac0
André Sampaio <andresampaio@id.uff.br>
2023-05-24 17:17:08 -0300

    Get books with pagination

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import ErroBase from "../erros/ErroBase.js";
 import ErroValidacao from "../erros/ErroValidacao.js";
-import NaoEncontrado from "../erros/NaoEncontrado.js";
 import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 
 // eslint-disable-next-line no-unused-vars
@@ -13,7 +12,7 @@ function manipuladorDeErros(erro,req,res, next){
    
     new ErroValidacao(erro).enviarResposta(res);
   }
-  else if(erro instanceof NaoEncontrado){
+  else if(erro instanceof ErroBase){
     erro.enviarResposta(res);
   }
   else{
